9a37d97393c13f730dbed7628944b59bf47ba172
root <veckert@uccs.edu>
2022-11-21 17:53:00 +0000

    modified project to fail tests

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
     #this line validates the presence of a title and a description in a project object
     #in other words, it makes sure the project fields actually have components
-    validates :title, :description, presence: true
+    #validates :title, :description, presence: true
 end
